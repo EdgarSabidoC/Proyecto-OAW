c1c20caff98b586da9be29d5c6868be22e96ac82
carlosruiz01 <card_@hotmail.com>
2023-03-02 01:51:48 -0600

    Se añadieron funcionalidades al proyecto para la busqueda en la base de datos en el apartado del FrontEnd.

--- application/models/rssReader_model.php
+++ application/models/rssReader_model.php
@@ -19,10 +19,28 @@
 				return $this->items;
 		}
 
+		// Obtiene todas las noticias/artículos de la DB:
+		public function search_items($text) {
+			$sql = "SELECT * FROM feed WHERE (title LIKE '%".$text."%' OR description LIKE '%".$text."%');";
+			$query = $this->db->query($sql);
+			while ($rows=$query->fetch_assoc()) {
+				$this->items[]=$rows;
+			}
+			return $this->items;
+	}
+
 		// Almacena en la BD los items:
 		public function set_item($title, $date, $description, $permalink, $categories) {
 			$sql = "INSERT INTO feed (title, date, description, permalink, categories)
 							VALUES ('{$title}', '{$date}', '{$description}', '{$permalink}', '{$categories}')";
+			if ($this->db->query($sql)) {
+				return true;
+			}
+			return false;
+		}
+
+		public function delete_items() {
+			$sql = "DELETE FROM feed";
 			if ($this->db->query($sql)) {
 				return true;
 			}
