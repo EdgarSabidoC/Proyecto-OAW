fbedb5ab8cc556ed509803dbf861fb3fac7918a8
Edgar Sabido C <edgar.sabido17@gmail.com>
2023-03-18 01:44:20 -0600

    Se modificó: Comportamiento de botón Borrar

--- application_v1/controllers/rss_update.php
+++ application_v1/controllers/rss_update.php
@@ -2,14 +2,14 @@
 	if (!isset($_COOKIE['urls'])) {
 		die;
 	}
-	$urls = $_COOKIE['urls'];
-	echo "<script>console.log('{$urls}')</script>";
+	$urls = $_COOKIE['urls']; // Se guardan las url de la cookie.
 
 	require_once("rss_delete.php"); // Se elimina la base de datos.
-
 	require_once("rss_storage.php");
+
 	$urls = str_replace('|', PHP_EOL, $urls);
 	$urls = explode(PHP_EOL, $urls);
+	require_once("cookies.php"); // Se genera de nuevo la cookie.
 
 	foreach ($urls as $url) {
 		rss_storage(trim($url));
